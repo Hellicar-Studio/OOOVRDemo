a4e14d3fa78b74dfb31b29fa6c55ecfe476d3e3c
Bentleyj <jgbentley10@gmail.com>
2017-08-11 11:13:16 +0100

    Add BW shader and shader applier script to work with only that shader for now

---
+++ Assets/Scripts/Character/CharacterFilter.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CharacterFilter : MonoBehaviour
+{
+
+    public float intensity;
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/BWDiffuse"));
+    }
+
+    public void setShader(Shader s)
+    {
+        material = new Material(s);
+    }
+
+    // Postprocess the image
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (intensity == 0)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, material);
+    }
+}
