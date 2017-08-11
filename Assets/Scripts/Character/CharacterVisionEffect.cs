8f6e5fbec069fb360cab58ac6a3e607e9b1abd64
Bentleyj <jgbentley10@gmail.com>
2017-08-11 15:10:54 +0100

    abstract base functionality of vision effect into a parent class

---
+++ Assets/Scripts/Character/CharacterVisionEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterVisionEffect : MonoBehaviour {
+
+    protected Material material;
+    public string shaderName;
+    // Use this for initialization
+    protected void Awake()
+    {
+        material = new Material(Shader.Find(shaderName));
+    }
+
+    // Postprocess the image
+    protected void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        setUniforms();
+        Graphics.Blit(source, destination, material);
+    }
+
+    protected virtual void setUniforms()
+    {
+
+    }
+}
--- Assets/Scripts/Character/CharacterVisionEffects/BMOVision.cs
+++ Assets/Scripts/Character/CharacterVisionEffects/BMOVision.cs
@@ -2,37 +2,14 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class BMOVision : MonoBehaviour
+public class BMOVision : CharacterVisionEffect
 {
-
     //public float intensity;
     public float deltaX, deltaY;
-    private Material material;
 
-    // Creates a private material used to the effect
-    void Awake()
+    protected override void setUniforms()
     {
-        material = new Material(Shader.Find("Hidden/BMOVision"));
-    }
-
-    public void setShader(Shader s)
-    {
-        material = new Material(s);
-    }
-
-    // Postprocess the image
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        //if (intensity == 0)
-        //{
-        //    Graphics.Blit(source, destination);
-        //    return;
-        //}
-
-        //material.SetFloat("_bwBlend", );
         material.SetFloat("_DeltaX", deltaX);
         material.SetFloat("_DeltaY", deltaY);
-
-        Graphics.Blit(source, destination, material);
     }
 }
