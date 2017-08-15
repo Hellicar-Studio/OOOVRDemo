602b5974744614626b0bca8ac7950301cf4c25f9
Bentleyj <jgbentley10@gmail.com>
2017-08-15 11:24:44 +0100

    Add PBG filter and modify jake vision slightly

---
+++ Assets/Scripts/Character/CharacterVisionEffects/PBGVision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PBGVision : CharacterVisionEffect {
+
+	public float vignettePower;
+	public Color tint;
+
+	public override void setUniforms()
+	{
+		material.SetColor("_Tint", tint);
+		material.SetFloat("_VignettePower", vignettePower);
+	}
+}
