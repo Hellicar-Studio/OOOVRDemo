5fab4c1663fd0be001c0170abde6b374c07ec60f
Bentleyj <jgbentley10@gmail.com>
2017-08-17 15:56:35 +0100

    Remove Reset Functionality from ForwardMovers and add it to a new class BananaManager

--- Assets/Scripts/Character/ForwardMover.cs
+++ Assets/Scripts/Character/ForwardMover.cs
@@ -8,20 +8,6 @@ public class ForwardMover : MonoBehaviour
 	public float speed = 2f;
 	public float maxDistance = 90f;
 
-	private Vector3 origin;
-
-	void Start()
-	{
-		origin = transform.position;
-	}
-
-	void Update()
-	{
-		float dist = Vector3.Distance(transform.position, origin);
-		if (dist > maxDistance)
-			transform.position = origin;
-	}
-
 	void FixedUpdate()
 	{
 		rigid.velocity = transform.forward * speed;
