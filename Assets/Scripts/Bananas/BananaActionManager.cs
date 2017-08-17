54933e9c8fb6f30fde4b49088c62431b36cce693
Bentleyj <jgbentley10@gmail.com>
2017-08-17 15:36:12 +0100

    Make Bananas work like their own little ActionManagers so they call the same stuff when they bump in to eachother

---
+++ Assets/Scripts/Bananas/BananaActionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaActionManager : ActionManager {
+
+	public CharacterManager characterManager;
+
+	public void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.tag == "Banana Guard")
+			ExecuteLookInteraction(characterManager);
+	}
+
+	void Update()
+	{
+		if (characterManager.characterAnimation.IsInTransition(0) && characterManager.characterAnimation.IsNameNextState(0, "run"))
+		{
+			characterManager.characterMover.enableIdleState();
+			canAnimationPlay = true;
+		}
+	}
+}
