cebd2fb5f6f2414cd5623a78e80dade5709b3b23
Bentleyj <jgbentley10@gmail.com>
2017-08-18 20:01:22 +0100

    Make BMO hittable again and add colorMaterialSetter

---
+++ Assets/Scripts/MaterialColorSetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialColorSetter : MonoBehaviour {
+
+	public Material material;
+
+	public string uniform;
+
+	public Color color;
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		material.SetColor(uniform, color);
+	}
+}
