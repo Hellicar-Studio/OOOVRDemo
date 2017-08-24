91cf80b847f0d724e46b9aece606cc1266441151
Bentleyj <jgbentley10@gmail.com>
2017-08-24 15:33:27 +0100

    Add mounted character tracking for different sounds from different perspectives

--- Assets/Scripts/Character/CharacterSound.cs
+++ Assets/Scripts/Character/CharacterSound.cs
@@ -5,11 +5,39 @@ using UnityEngine;
 public class CharacterSound : MonoBehaviour
 {
 	public AudioSource characterAudioSource;
-	public AudioClip[] characterSounds;
+	public AudioClip[] defaultSounds;
+	public AudioClip[] sounds;
+	public CharacterManager[] characters;
+	Dictionary<CharacterManager, AudioClip> characterSounds;
 
+	void Awake()
+	{
+		characterSounds = new Dictionary<CharacterManager, AudioClip>();
+
+		if(sounds.Length == characters.Length)
+		{
+			for(int i = 0; i > sounds.Length; i++)
+			{
+				characterSounds.Add(characters[i], sounds[i]);
+			}
+		}
+		else
+		{
+			Debug.Log("Characters and Sounds do not match!");
+		}
+	}
 	private AudioClip GetRandomCharacterSound()
 	{
-		return characterSounds[Random.Range(0, characterSounds.Length - 1)];
+		return defaultSounds[Random.Range(0, defaultSounds.Length)];
+	}
+
+	private AudioClip GetCharacterSpecificSound(CharacterManager currentManager)
+	{
+		if(characterSounds[currentManager] != null)
+		{
+			return characterSounds[currentManager];
+		}
+		return GetRandomCharacterSound();
 	}
 
 	public void Play()
@@ -18,6 +46,15 @@ public class CharacterSound : MonoBehaviour
 			return;
 
 		characterAudioSource.clip = GetRandomCharacterSound();
+		characterAudioSource.Play();
+	}
+
+	public void Play(CharacterManager currentCharacter)
+	{
+		if (characterAudioSource.isPlaying)
+			return;
+
+		characterAudioSource.clip = GetCharacterSpecificSound(currentCharacter);
 		characterAudioSource.Play();
 	}
 }
